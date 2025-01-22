4e06db89979b7cbdd5bae5282503c1f51f9db811
Soner Sayakci <s.sayakci@gmail.com>
2025-01-22 10:58:41 +0100

    feat: enhance project creation to support dev versions

--- cmd/project/project_create.go
+++ cmd/project/project_create.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"regexp"
 	"sort"
 	"strings"
 	"text/template"
@@ -75,6 +76,8 @@ var projectCreateCmd = &cobra.Command{
 
 		if result == "latest" {
 			chooseVersion = filteredVersions[0].String()
+		} else if strings.HasPrefix(result, "dev-") {
+			chooseVersion = result
 		} else {
 			for _, release := range filteredVersions {
 				if release.String() == result {
@@ -94,7 +97,7 @@ var projectCreateCmd = &cobra.Command{
 
 		logging.FromContext(cmd.Context()).Infof("Setting up Shopware %s", chooseVersion)
 
-		composerJson, err := generateComposerJson(chooseVersion, strings.Contains(chooseVersion, "rc"))
+		composerJson, err := generateComposerJson(cmd.Context(), chooseVersion, strings.Contains(chooseVersion, "rc"))
 		if err != nil {
 			return err
 		}
@@ -205,17 +208,17 @@ func fetchAvailableShopwareVersions(ctx context.Context) ([]string, error) {
 	return releases, nil
 }
 
-func generateComposerJson(version string, rc bool) (string, error) {
+func generateComposerJson(ctx context.Context, version string, rc bool) (string, error) {
 	tplContent, err := template.New("composer.json").Parse(`{
     "name": "shopware/production",
     "license": "MIT",
     "type": "project",
     "require": {
         "composer-runtime-api": "^2.0",
-        "shopware/administration": "*",
+        "shopware/administration": "{{ .DependingVersions }}",
         "shopware/core": "{{ .Version }}",
-        "shopware/elasticsearch": "*",
-        "shopware/storefront": "*",
+        "shopware/elasticsearch": "{{ .DependingVersions }}",
+        "shopware/storefront": "{{ .DependingVersions }}",
         "symfony/flex": "~2"
     },
     "repositories": [
@@ -279,16 +282,69 @@ func generateComposerJson(version string, rc bool) (string, error) {
 
 	buf := new(bytes.Buffer)
 
-	err = tplContent.Execute(buf, struct {
-		Version string
-		RC      bool
-	}{
-		Version: version,
-		RC:      rc,
+	dependingVersions := "*"
+
+	if strings.HasPrefix(version, "dev-") {
+		fallbackVersion, err := getLatestFallbackVersion(ctx, strings.TrimPrefix(version, "dev-"))
+		if err != nil {
+			return "", err
+		}
+
+		if strings.HasPrefix(version, "dev-6") {
+			version = strings.TrimPrefix(version, "dev-") + "-dev"
+		}
+
+		version = fmt.Sprintf("%s as %s.9999999-dev", version, fallbackVersion)
+		dependingVersions = version
+	}
+
+	err = tplContent.Execute(buf, map[string]interface{}{
+		"Version":           version,
+		"DependingVersions": dependingVersions,
+		"RC":                rc,
 	})
 	if err != nil {
 		return "", err
 	}
 
 	return buf.String(), nil
+}
+
+var kernelFallbackRegExp = regexp.MustCompile(`(?m)SHOPWARE_FALLBACK_VERSION\s*=\s*'?"?(\d+\.\d+)`)
+
+func getLatestFallbackVersion(ctx context.Context, branch string) (string, error) {
+	r, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("https://raw.githubusercontent.com/shopware/core/refs/heads/%s/Kernel.php", branch), http.NoBody)
+	if err != nil {
+		return "", err
+	}
+
+	r.Header.Set("User-Agent", "shopware-cli")
+
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("could not fetch kernel.php from branch %s", branch)
+	}
+
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			logging.FromContext(context.Background()).Errorf("getLatestFallbackVersion: %v", err)
+		}
+	}()
+
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	matches := kernelFallbackRegExp.FindSubmatch(content)
+
+	if len(matches) < 2 {
+		return "", fmt.Errorf("could not determine shopware version")
+	}
+
+	return string(matches[1]), nil
 }
