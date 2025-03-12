1629c7428686e135e43fc23cd9e1eaa29b653abf
Soner Sayakci <s.sayakci@gmail.com>
2025-03-12 14:18:13 +0100

    feat: auto generate extension config file json schema

--- extension/config.go
+++ extension/config.go
@@ -5,61 +5,112 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/invopop/jsonschema"
 	"github.com/shopware/shopware-cli/internal/changelog"
+	orderedmap "github.com/wk8/go-ordered-map/v2"
 
 	"gopkg.in/yaml.v3"
 )
 
 type ConfigBuild struct {
-	ExtraBundles              []ConfigExtraBundle `yaml:"extraBundles,omitempty"`
-	ShopwareVersionConstraint string              `yaml:"shopwareVersionConstraint,omitempty"`
-	Zip                       struct {
-		Composer struct {
-			Enabled          bool     `yaml:"enabled"`
-			BeforeHooks      []string `yaml:"before_hooks,omitempty"`
-			AfterHooks       []string `yaml:"after_hooks,omitempty"`
-			ExcludedPackages []string `yaml:"excluded_packages,omitempty"`
-		} `yaml:"composer"`
-		Assets struct {
-			Enabled                    bool     `yaml:"enabled"`
-			BeforeHooks                []string `yaml:"before_hooks,omitempty"`
-			AfterHooks                 []string `yaml:"after_hooks,omitempty"`
-			EnableESBuildForAdmin      bool     `yaml:"enable_es_build_for_admin"`
-			EnableESBuildForStorefront bool     `yaml:"enable_es_build_for_storefront"`
-			DisableSass                bool     `yaml:"es_build_disable_sass"`
-			NpmStrict                  bool     `yaml:"npm_strict"`
-		} `yaml:"assets"`
-		Pack struct {
-			Excludes struct {
-				Paths []string `yaml:"paths,omitempty"`
-			} `yaml:"excludes,omitempty"`
-			BeforeHooks []string `yaml:"before_hooks,omitempty"`
-		} `yaml:"pack"`
-	} `yaml:"zip"`
+	// ExtraBundles can be used to declare additional bundles to be considered for building
+	ExtraBundles []ConfigExtraBundle `yaml:"extraBundles,omitempty"`
+	// Override the shopware version constraint for building, can be used to specify the version of the shopware to use for building
+	ShopwareVersionConstraint string `yaml:"shopwareVersionConstraint,omitempty"`
+	// Configuration for zipping
+	Zip ConfigBuildZip `yaml:"zip"`
+}
+
+// Configuration for zipping
+type ConfigBuildZip struct {
+	// Configuration for composer
+	Composer ConfigBuildZipComposer `yaml:"composer"`
+	// Configuration for assets
+	Assets ConfigBuildZipAssets `yaml:"assets"`
+	// Configuration for packing
+	Pack ConfigBuildZipPack `yaml:"pack"`
+}
+
+type ConfigBuildZipComposer struct {
+	// When enabled, a vendor folder will be created in the zip build
+	Enabled bool `yaml:"enabled"`
+	// Commands to run before the composer install
+	BeforeHooks []string `yaml:"before_hooks,omitempty"`
+	// Commands to run after the composer install
+	AfterHooks []string `yaml:"after_hooks,omitempty"`
+	// Composer packages to be excluded from the zip build
+	ExcludedPackages []string `yaml:"excluded_packages,omitempty"`
+}
+
+type ConfigBuildZipAssets struct {
+	// When enabled, the shopware-cli build the assets
+	Enabled bool `yaml:"enabled"`
+	// Commands to run before the assets build
+	BeforeHooks []string `yaml:"before_hooks,omitempty"`
+	// Commands to run after the assets build
+	AfterHooks []string `yaml:"after_hooks,omitempty"`
+	// When enabled, builtin esbuild will be used for the admin assets
+	EnableESBuildForAdmin bool `yaml:"enable_es_build_for_admin"`
+	// When enabled, builtin esbuild will be used for the storefront assets
+	EnableESBuildForStorefront bool `yaml:"enable_es_build_for_storefront"`
+	// When disabled, builtin sass support will be disabled
+	DisableSass bool `yaml:"disable_sass"`
+	// When enabled, npm will install only production dependencies
+	NpmStrict bool `yaml:"npm_strict"`
+}
+
+type ConfigBuildZipPackExcludes struct {
+	// Paths to exclude from the zip build
+	Paths []string `yaml:"paths,omitempty"`
+}
+
+type ConfigBuildZipPack struct {
+	// Excludes can be used to exclude files from the zip build
+	Excludes ConfigBuildZipPackExcludes `yaml:"excludes,omitempty"`
+	// Commands to run before the pack
+	BeforeHooks []string `yaml:"before_hooks,omitempty"`
 }
 
 type ConfigExtraBundle struct {
+	// Path to the bundle, relative from the extension root (src folder)
 	Path string `yaml:"path"`
+	// Name of the bundle, if empty the folder name of path will be used
 	Name string `yaml:"name"`
 }
 
 type ConfigStore struct {
-	Availabilities                      *[]string                          `yaml:"availabilities"`
-	DefaultLocale                       *string                            `yaml:"default_locale"`
-	Localizations                       *[]string                          `yaml:"localizations"`
-	Categories                          *[]string                          `yaml:"categories"`
-	Type                                *string                            `yaml:"type"`
-	Icon                                *string                            `yaml:"icon"`
-	AutomaticBugfixVersionCompatibility *bool                              `yaml:"automatic_bugfix_version_compatibility"`
-	Description                         ConfigTranslated[string]           `yaml:"description"`
-	InstallationManual                  ConfigTranslated[string]           `yaml:"installation_manual"`
-	Tags                                ConfigTranslated[[]string]         `yaml:"tags"`
-	Videos                              ConfigTranslated[[]string]         `yaml:"videos"`
-	Highlights                          ConfigTranslated[[]string]         `yaml:"highlights"`
-	Features                            ConfigTranslated[[]string]         `yaml:"features"`
-	Faq                                 ConfigTranslated[[]ConfigStoreFaq] `yaml:"faq"`
-	Images                              *[]ConfigStoreImage                `yaml:"images,omitempty"`
-	ImageDirectory                      *string                            `yaml:"image_directory,omitempty"`
+	// Specifies the visibility in stores.
+	Availabilities *[]string `yaml:"availabilities" jsonschema:"enum=German,enum=International"`
+	// Specifies the default locale.
+	DefaultLocale *string `yaml:"default_locale" jsonschema:"enum=de_DE,enum=en_GB"`
+	// Specifies the languages the extension is translated.
+	Localizations *[]string `yaml:"localizations" jsonschema:"enum=de_DE,enum=en_GB,enum=bs_BA,enum=bg_BG,enum=cs_CZ,enum=da_DK,enum=de_CH,enum=el_GR,enum=en_US,enum=es_ES,enum=fi_FI,enum=fr_FR,enum=hi_IN,enum=hr_HR,enum=hu_HU,enum=hy,enum=id_ID,enum=it_IT,enum=ko_KR,enum=lv_LV,enum=ms_MY,enum=nl_NL,enum=pl_PL,enum=pt_BR,enum=pt_PT,enum=ro_RO,enum=ru_RU,enum=sk_SK,enum=sl_SI,enum=sr_RS,enum=sv_SE,enum=th_TH,enum=tr_TR,enum=uk_UA,enum=vi_VN,enum=zh_CN,enum=zh_TW"`
+	// Specifies the categories.
+	Categories *[]string `yaml:"categories" jsonschema:"enum=Administration,enum=SEOOptimierung,enum=Bonitaetsprüfung,enum=Rechtssicherheit,enum=Auswertung,enum=KommentarFeedback,enum=Tracking,enum=Integration,enum=PreissuchmaschinenPortale,enum=Warenwirtschaft,enum=Versand,enum=Bezahlung,enum=StorefrontDetailanpassungen,enum=Sprache,enum=Suche,enum=HeaderFooter,enum=Detailseite,enum=MenueKategorien,enum=Bestellprozess,enum=KundenkontoPersonalisierung,enum=Sonderfunktionen,enum=Themes,enum=Branche,enum=Home+Furnishings,enum=FashionBekleidung,enum=GartenNatur,enum=KosmetikGesundheit,enum=EssenTrinken,enum=KinderPartyGeschenke,enum=SportLifestyleReisen,enum=Bauhaus,enum=Elektronik,enum=Geraete,enum=Heimkueche,enum=Hobby,enum=Kueche,enum=Lebensmittel,enum=Medizin,enum=Mode,enum=Musik,enum=Spiel,enum=Technik,enum=Umweltschutz,enum=Wohnen,enum=Zubehoer"`
+	// Specifies the type of the extension.
+	Type *string `yaml:"type" jsonschema:"enum=extension,enum=theme"`
+	// Specifies the Path to the icon (128x128 px) for store.
+	Icon *string `yaml:"icon"`
+	// Specifies whether the extension should automatically be set compatible with Shopware bugfix versions.
+	AutomaticBugfixVersionCompatibility *bool `yaml:"automatic_bugfix_version_compatibility"`
+	// Specifies the description of the extension in store.
+	Description ConfigTranslated[string] `yaml:"description"`
+	// Installation manual of the extension in store.
+	InstallationManual ConfigTranslated[string] `yaml:"installation_manual"`
+	// Specifies the tags of the extension.
+	Tags ConfigTranslated[[]string] `yaml:"tags"`
+	// Specifies the links of YouTube-Videos to show or describe the extension.
+	Videos ConfigTranslated[[]string] `yaml:"videos"`
+	// Specifies the highlights of the extension.
+	Highlights ConfigTranslated[[]string] `yaml:"highlights"`
+	// Specifies the features of the extension.
+	Features ConfigTranslated[[]string] `yaml:"features"`
+	// Specifies Frequently Asked Questions for the extension.
+	Faq ConfigTranslated[[]ConfigStoreFaq] `yaml:"faq"`
+	// Specifies images for the extension in the store.
+	Images *[]ConfigStoreImage `yaml:"images,omitempty"`
+	// Specifies the directory where the images are located.
+	ImageDirectory *string `yaml:"image_directory,omitempty"`
 }
 
 type Translatable interface {
@@ -77,10 +128,14 @@ type ConfigStoreFaq struct {
 }
 
 type ConfigStoreImage struct {
-	File     string                   `yaml:"file"`
+	// File path to image relative from root of the extension
+	File string `yaml:"file"`
+	// Specifies whether the image is active in the language.
 	Activate ConfigStoreImageActivate `yaml:"activate"`
-	Preview  ConfigStoreImagePreview  `yaml:"preview"`
-	Priority int                      `yaml:"priority"`
+	// Specifies whether the image is a preview in the language.
+	Preview ConfigStoreImagePreview `yaml:"preview"`
+	// Specifies the order of the image ascending the given priority.
+	Priority int `yaml:"priority"`
 }
 
 type ConfigStoreImageActivate struct {
@@ -93,7 +148,9 @@ type ConfigStoreImagePreview struct {
 	English bool `yaml:"en"`
 }
 
+// ConfigValidation is used to configure the extension validation.
 type ConfigValidation struct {
+	// Ignore items from the validation.
 	Ignore ConfigValidationList `yaml:"ignore,omitempty"`
 }
 
@@ -108,8 +165,10 @@ func (c *ConfigValidationList) Identifiers() []string {
 }
 
 type ConfigValidationIgnoreItem struct {
+	// The identifier of the item to ignore.
 	Identifier string `yaml:"identifier"`
-	Path       string `yaml:"path,omitempty"`
+	// Optional to ignore only a specific path.
+	Path string `yaml:"path,omitempty"`
 }
 
 func (c *ConfigValidationIgnoreItem) UnmarshalYAML(value *yaml.Node) error {
@@ -133,11 +192,41 @@ func (c *ConfigValidationIgnoreItem) UnmarshalYAML(value *yaml.Node) error {
 	return nil
 }
 
+func (c ConfigValidationIgnoreItem) JSONSchema() *jsonschema.Schema {
+	ordMap := orderedmap.New[string, *jsonschema.Schema]()
+
+	ordMap.Set("identifier", &jsonschema.Schema{
+		Type:        "string",
+		Description: "The identifier of the item to ignore.",
+	})
+
+	ordMap.Set("path", &jsonschema.Schema{
+		Type:        "string",
+		Description: "The path of the item to ignore.",
+	})
+
+	return &jsonschema.Schema{
+		OneOf: []*jsonschema.Schema{
+			{
+				Type:       "object",
+				Properties: ordMap,
+			},
+			{
+				Type: "string",
+			},
+		},
+	}
+}
+
 type Config struct {
-	FileName   string
-	Store      ConfigStore      `yaml:"store"`
-	Build      ConfigBuild      `yaml:"build"`
-	Changelog  changelog.Config `yaml:"changelog"`
+	FileName string `jsonschema:"-"`
+	// Store is the store configuration of the extension.
+	Store ConfigStore `yaml:"store"`
+	// Build is the build configuration of the extension.
+	Build ConfigBuild `yaml:"build"`
+	// Changelog is the changelog configuration of the extension.
+	Changelog changelog.Config `yaml:"changelog"`
+	// Validation is the validation configuration of the extension.
 	Validation ConfigValidation `yaml:"validation"`
 }
 
--- scripts/schema.go
+++ scripts/schema.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"reflect"
+	"regexp"
+	"strings"
 
 	"github.com/invopop/jsonschema"
+	"github.com/shopware/shopware-cli/extension"
 	"github.com/shopware/shopware-cli/shop"
 )
+
+var genericTypeRegex = regexp.MustCompile(`^(.+?)\[(.+)]$`)
+
+func getSimpleTypeName(name string) string {
+	parts := strings.Split(name, ".")
+	return parts[len(parts)-1]
+}
+
+func getGenericName(name string) string {
+	if matches := genericTypeRegex.FindStringSubmatch(name); matches != nil {
+		parent := getSimpleTypeName(matches[1])
+		child := getGenericName(matches[2])
+		return fmt.Sprintf("%s[%s]", parent, child)
+	}
+	return getSimpleTypeName(name)
+}
+
+func nameGenerics(r reflect.Type) string {
+	return getGenericName(r.Name())
+}
 
 func generateProjectSchema() error {
 	r := new(jsonschema.Reflector)
@@ -31,8 +56,35 @@ func generateProjectSchema() error {
 	return nil
 }
 
+func generateExtensionSchema() error {
+	r := jsonschema.Reflector{Namer: nameGenerics}
+	r.FieldNameTag = "yaml"
+	r.RequiredFromJSONSchemaTags = true
+
+	if err := r.AddGoComments("github.com/shopware/shopware-cli", "./extension"); err != nil {
+		return err
+	}
+
+	schema := r.Reflect(&extension.Config{})
+
+	bytes, err := json.MarshalIndent(schema, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile("extension/shopware-extension-schema.json", bytes, 0o644); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 	if err := generateProjectSchema(); err != nil {
+		panic(err)
+	}
+
+	if err := generateExtensionSchema(); err != nil {
 		panic(err)
 	}
 }
